6661eb8ac46663f318877479609df4ef7991d31d
nicolas <muguruzanicolas84@gmail.com>
2022-11-01 18:29:07 -0300

    cotizar

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+
+const ContenedorHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+//Estos son Styled Components, te permite crear componentes estilizados usando
+//la librería @emotion/styled.
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
+const Header = ({titulo}) => {
+    return ( 
+        <ContenedorHeader>
+            <TextoHeader>{titulo}</TextoHeader>
+        </ContenedorHeader>
+    );
+}
+
+Header.propTypes = {
+    titulo: PropTypes.string.isRequired
+}
+
+export default Header;
